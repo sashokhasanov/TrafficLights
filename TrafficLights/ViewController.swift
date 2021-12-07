680fa8a915153360ae736f13699051cc96a3e193
Alexander Khasanov <sashokhasanov@mail.ru>
2021-12-07 17:15:50 +0300

    App functionality

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchStateButton: UIButton!
+    
+    @IBOutlet weak var trafficLightsStackView: UIStackView!
+    
+    var currentLightIndex = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        switchStateButton.setTitle(startTitle, for: .normal)
+        
+        trafficLightsStackView.subviews.forEach() {
+            $0.layer.cornerRadius = $0.bounds.width * 0.45
+        }
     }
 
-
+    @IBAction func switchStateButtonPressed() {
+        
+        let title = currentLightIndex < 0 ? startTitle : nextTitle
+        switchStateButton.setTitle(title, for: .normal)
+        
+        currentLightIndex += 1
+        currentLightIndex %= trafficLightsStackView.subviews.count
+        
+        trafficLightsStackView.subviews.enumerated().forEach() {
+            $0.element.alpha =
+                $0.offset == currentLightIndex ? turnOnAlpha : turnOffAlpha
+        }
+    }
+    
+    private let startTitle = "START"
+    private let nextTitle = "NEXT"
+    
+    private let turnOffAlpha = CGFloat(0.5)
+    private let turnOnAlpha = CGFloat(1)
+    
 }
 
