608a8744ee8977fa9d24a1d1b75dc2414c8c0d14
Alexander Khasanov <sashokhasanov@mail.ru>
2021-12-07 20:33:55 +0300

    refactor & minor fixes

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var switchStateButton: UIButton!
-    
     @IBOutlet weak var trafficLightsStackView: UIStackView!
     
-    var currentLightIndex = -1
+    private let startTitle = "START"
+    private let nextTitle = "NEXT"
+    
+    private let turnOffAlpha = CGFloat(0.3)
+    private let turnOnAlpha = CGFloat(1)
+    
+    private var currentLightIndex = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,28 +27,21 @@ class ViewController: UIViewController {
         
         trafficLightsStackView.subviews.forEach() {
             $0.layer.cornerRadius = $0.bounds.width * 0.5
+            $0.alpha = turnOffAlpha
         }
     }
 
     @IBAction func switchStateButtonPressed() {
+        currentLightIndex += 1
+        currentLightIndex %= trafficLightsStackView.subviews.count
         
         let title = currentLightIndex < 0 ? startTitle : nextTitle
         switchStateButton.setTitle(title, for: .normal)
         
-        currentLightIndex += 1
-        currentLightIndex %= trafficLightsStackView.subviews.count
-        
         trafficLightsStackView.subviews.enumerated().forEach() {
-            $0.element.alpha =
-                $0.offset == currentLightIndex ? turnOnAlpha : turnOffAlpha
+            let alphaValue = $0.offset == currentLightIndex ? turnOnAlpha : turnOffAlpha
+            $0.element.alpha = alphaValue
         }
     }
-    
-    private let startTitle = "START"
-    private let nextTitle = "NEXT"
-    
-    private let turnOffAlpha = CGFloat(0.5)
-    private let turnOnAlpha = CGFloat(1)
-    
 }
 
